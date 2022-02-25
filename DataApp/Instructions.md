3d4f560837ed3f3a0feb5d9f4a9b57cb720b013f
Kevin <Kberrio@users.noreply.github.com>
2022-02-25 13:40:24 -0500

    updating files

---
+++ DataApp/Instructions.md
@@ -0,0 +1,6 @@
+We have index.js --> this is the server
+The  purpose of the server right now is to host static files (index.html)
+
+What do we want to do?
+    Save to database
+    Authenticate
--- DataApp/index.js
+++ DataApp/index.js
@@ -1,5 +1,18 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3030;
 app.listen(port, () => console.log(`Listening at ${port}`));
 app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+app.post('/api', (req, res) => {
+	console.log('I got a request!');
+	console.log(req.body);
+	const data = request.body;
+
+	res.json({
+		status: 'success',
+		latitude: data.lat,
+		longitude: data.lon,
+	});
+});
