8041446126d338debb28e74068ecefb2aa951ad2
Kevin <kberrio@gmail.com>
2023-01-01 23:34:43 -0600

    new files

---
+++ Basic Exercises/Exercise_01.js
@@ -0,0 +1,2 @@
+// Write a Node.js program to print "Hello, World!" to the console.
+console.log('Hello, World!');
---
+++ Basic Exercises/Exercise_02.js
@@ -0,0 +1,2 @@
+// Write a Node.js program to print the current date and time to the console.
+console.log(new Date().toString());
