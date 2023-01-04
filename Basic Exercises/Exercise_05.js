709db21f8babc7a3402c326f81b3e46f9d907700
Kevin <kberrio@gmail.com>
2023-01-03 23:39:05 -0600

    new files

---
+++ Basic Exercises/Exercise_05.js
@@ -0,0 +1,4 @@
+//Write a Node.js program to convert temperatures from Fahrenheit to Celsius.
+function fahrenheitToCelsius(f) {
+	return (f - 32) * (5 / 9);
+}
---
+++ Basic Exercises/Exercise_06.js
@@ -0,0 +1,2 @@
+//Write a Node.js program to generate a random number between 1 and 10.
+const randomNumber = Math.floor(Math.random() * 10) + 1;
